9a93ec6d64581caa9ad9fe80e7c738674b05505c
Chris Schimetschka <chris.schimetschka@gmail.com>
2023-01-25 12:30:39 +0100

    chore: setup base project and first components

---
+++ src/components/Order.tsx
@@ -0,0 +1,29 @@
+const Order: React.FC = (): JSX.Element => {
+	return (
+		<form className="my-4 p-2 container grid grid-rows-3 place-items-center gap-3 mx-auto max-w-screen-lg">
+			<input
+				className="block container rounded-full border border-solid border-gray-400 py-3 px-4 text-sm"
+				placeholder="Gebe deine Email-Adresse an"
+			/>
+			<div className="grid grid-cols-2 gap-2 container">
+				<select className="block container rounded-full border border-solid border-gray-400 py-3 px-4 text-sm">
+					<option>XS</option>
+					<option>S</option>
+					<option>M</option>
+					<option>L</option>
+					<option>XL</option>
+				</select>
+				<select className="block container rounded-full border border-solid border-gray-400 py-3 px-4 text-sm">
+					<option>1</option>
+					<option>2</option>
+					<option>3</option>
+				</select>
+			</div>
+			<button className="block text-sm py-3 px-4 container rounded-full border border-solid border-[#7AAD7A] bg-[#7AAD7A] text-white">
+				Bestellen
+			</button>
+		</form>
+	);
+};
+
+export default Order;

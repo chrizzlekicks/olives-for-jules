265cd6186d1fced05e3e296f17c14a9991ab8b7e
Chris Schimetschka <chris.schimetschka@gmail.com>
2023-01-27 13:47:05 +0100

    feat: parallax effect added

---
+++ src/components/Intro.tsx
@@ -0,0 +1,52 @@
+import * as React from 'react';
+import { Parallax, ParallaxLayer } from '@react-spring/parallax';
+import { useScroll, animated } from '@react-spring/web';
+
+import logo from '../assets/olivesforjules_logonocircle.svg';
+
+const Intro: React.FC = (): JSX.Element => {
+	const containerRef = React.useRef<HTMLDivElement>(null!);
+	const { scrollYProgress } = useScroll({
+		container: containerRef,
+		default: {
+			immediate: true,
+		},
+	});
+	const flex = {
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'center',
+	};
+	return (
+		<div className="w-full h-screen inset-0 bg-secondary">
+			<Parallax pages={3}>
+				<ParallaxLayer offset={0} sticky={{ start: 0, end: 2 }} style={flex}>
+					<span className="w-72 sm:w-1/2 border-none rounded-full aspect-square bg-primary"></span>
+				</ParallaxLayer>
+				<ParallaxLayer
+					offset={1}
+					sticky={{ start: 1, end: 2 }}
+					speed={1.5}
+					style={flex}
+				>
+					<img className="sm:w-1/2 aspect-square" src={logo} alt="face" />
+				</ParallaxLayer>
+				<ParallaxLayer offset={2} style={flex}>
+					<div
+						ref={containerRef}
+						className="w-[2500px] h-[2500px] overflow-hidden absolute bg-transparent flex justify-center items-center"
+					>
+						<animated.span
+							style={{
+								clipPath: scrollYProgress.to((val) => `circle(${val * 170}%)`),
+							}}
+							className="w-full border-none rounded-full aspect-square bg-primary"
+						></animated.span>
+					</div>
+				</ParallaxLayer>
+			</Parallax>
+		</div>
+	);
+};
+
+export default Intro;

ad4e3a03a4763d59f54f10e0a38b6d0476618ab2
Chris Schimetschka <chris.schimetschka@gmail.com>
2023-02-02 17:53:53 +0100

    chore: changed typo size and modified some styling

--- src/components/Order.tsx
+++ src/components/Order.tsx
@@ -4,22 +4,22 @@ const Order: React.FC = (): JSX.Element => {
 	return (
 		<form className="my-4 container grid grid-rows-3 place-items-center gap-3 mx-auto max-w-screen-lg">
 			<input
-				className="block container rounded-full border border-solid border-gray-400 py-3 px-4 text-sm"
-				placeholder="Gebe deine Email-Adresse an"
+				className="block container rounded-full border border-solid border-gray-400 py-3 px-4"
+				placeholder="Gib deine Email-Adresse an."
 			/>
 			<div className="grid grid-cols-3 gap-2 container">
-				<select className="block container rounded-full border border-solid border-gray-400 py-3 px-4 text-sm">
+				<select className="block container rounded-full bg-white border border-solid border-gray-400 py-3 px-4">
 					<option>XS</option>
 					<option>S</option>
 					<option>M</option>
 					<option>L</option>
 					<option>XL</option>
 				</select>
-				<select className="block container rounded-full border border-solid border-gray-400 py-3 px-4 text-sm">
+				<select className="block container rounded-full bg-white border border-solid border-gray-400 py-3 px-4">
 					<option>Frau</option>
 					<option>Mann</option>
 				</select>
-				<select className="block container rounded-full border border-solid border-gray-400 py-3 px-4 text-sm">
+				<select className="block container rounded-full bg-white border border-solid border-gray-400 py-3 px-4">
 					<option>1</option>
 					<option>2</option>
 					<option>3</option>

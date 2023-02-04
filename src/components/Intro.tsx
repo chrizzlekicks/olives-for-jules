eff719259fe2515c9f778368d10ce09cef76a832
Chris Schimetschka <chris.schimetschka@gmail.com>
2023-02-04 18:32:56 +0100

    fix: last experimental fix for today

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -38,7 +38,7 @@ const Intro: React.FC = (): JSX.Element => {
 				<ParallaxLayer offset={2} style={flex}>
 					<div
 						ref={containerRef}
-						className="w-[3000px] h-screen-ios h-screen overflow-hidden absolute bg-transparent flex justify-center items-center"
+						className="w-[2500px] h-[2500px] overflow-hidden absolute bg-transparent flex justify-center items-center"
 					>
 						<animated.span
 							style={{

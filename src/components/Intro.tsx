2fdac10d27b8ad0476f8da39979be5b64335ec14
Chris Schimetschka <chris.schimetschka@gmail.com>
2023-02-04 10:47:53 +0100

    fix: adjusted animation duration --trigger when hovering

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -21,7 +21,7 @@ const Intro: React.FC = (): JSX.Element => {
 		<div className="w-full h-screen inset-0 bg-secondary">
 			<Parallax pages={3}>
 				<ParallaxLayer offset={0} sticky={{ start: 0, end: 2 }} style={flex}>
-					<span className="w-80 sm:w-1/2 border-none rounded-full aspect-square bg-primary motion-safe:animate-ping"></span>
+					<span className="w-80 sm:w-1/2 border-none rounded-full aspect-square bg-primary transition-transform hover:animate-ping"></span>
 				</ParallaxLayer>
 				<ParallaxLayer
 					offset={1}
@@ -30,7 +30,7 @@ const Intro: React.FC = (): JSX.Element => {
 					style={flex}
 				>
 					<img
-						className="w-80 sm:w-[47%] aspect-square transition-all motion-safe:animate-pulse"
+						className="w-80 sm:w-[47%] aspect-square transition-opacity hover:animate-pulse"
 						src={logo}
 						alt="face"
 					/>
